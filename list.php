cf51bf9c1a8ffdb8c6e7a8553b8799489f575b17
zyspec <zyspec@yahoo.com>
2020-07-03 14:03:33 -0500

    fix new icon alt text
    * minor code cleanup
    * switch to use GLOBALS[]
    * phpDocumentor updates

--- list.php
+++ list.php
@@ -1,38 +1,49 @@
 <?php
-/**
-* XOOPS - PHP Content Management System
-* Copyright (c) 2004 <http://www.xoops.org/>
-*
-* Module: edito 3.0
-* Licence : GPL
-* Authors :
-*           - solo (http://www.wolfpackclan.com/wolfactory)
-*			- DuGris (http://www.dugris.info)
-*/
+/*
+ You may not change or alter any portion of this comment or credits of
+ supporting developers from this source code or any supporting source code
+ which is considered copyrighted (c) material of the original comment or credit
+ authors.
 
-defined("XOOPS_ROOT_PATH") || exit("Restricted access");
+ This program is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+/**
+ * Module: Edito
+ *
+ * @package   \XoopsModules\Edito
+ * @copyright Copyright {@link https://xoops.org XOOPS Project}
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html GNU Public License
+ * @author    Solo (http://www.wolfpackclan.com/wolfactory)
+ * @author    DuGris (http://www.dugris.info)
+ * @author    XOOPS Module Development Team
+ * @link      https://github.com/XoopsModules25x/edito
+ */
+
+defined('XOOPS_ROOT_PATH') || exit('Restricted access');
 
 $count_list    = 0;
 $new_list      = '';
 $pop_list      = '';
 $readmore_list = '';
 
-$result         = "SELECT COUNT(*) FROM " . $xoopsDB->prefix($xoopsModule->dirname() . '_content' ) . " WHERE status > 2";
-list( $totals ) = $xoopsDB->fetchRow($xoopsDB->queryF($result));
+$result         = "SELECT COUNT(*) FROM " . $GLOBALS['xoopsDB']->prefix($GLOBALS['xoopsModule']->dirname() . '_content') . " WHERE status > 2";
+list( $totals ) = $GLOBALS['xoopsDB']->fetchRow($GLOBALS['xoopsDB']->queryF($result));
 
-$total_list = $totals/$xoopsModuleConfig["columns"];
-if( $totals > $xoopsModuleConfig['perpage'] ) {
-	$readmore_list = '<div style="text-align:right;"><a href="index.php?startart='.$xoopsModuleConfig['perpage'].'">' . _MD_EDITO_READMORE . '</a></div>';
+$total_list = $totals/$GLOBALS['xoopsModuleConfig']['columns'];
+if( $totals > $GLOBALS['xoopsModuleConfig']['perpage'] ) {
+	$readmore_list = '<div class="right"><a href="index.php?startart='.$GLOBALS['xoopsModuleConfig']['perpage'].'">' . _MD_EDITO_READMORE . '</a></div>';
 }
 
-$xoopsTpl->assign('readmore', $readmore_list);
+$GLOBALS['xoopsTpl']->assign('readmore', $readmore_list);
 
-$sql = "SELECT id, subject, groups, datesub, counter, meta FROM ".$xoopsDB->prefix($xoopsModule->dirname() . "_content")."
-		WHERE status > 2 ORDER BY " . $xoopsModuleConfig["order"];
+$sql = "SELECT id, subject, groups, datesub, counter, meta FROM ".$GLOBALS['xoopsDB']->prefix($GLOBALS['xoopsModule']->dirname() . "_content")."
+		WHERE status > 2 ORDER BY " . $GLOBALS['xoopsModuleConfig']['order'];
 
-$listing = $xoopsDB->queryF($sql, $xoopsModuleConfig['perpage'], 0 );
+$listing = $GLOBALS['xoopsDB']->queryF($sql, $GLOBALS['xoopsModuleConfig']['perpage'], 0 );
 
-while(list( $ids, $subject_list, $groups_list, $datesub_list, $counter_list, $meta_list) = $xoopsDB->fetchRow($listing)) {
+while(list( $ids, $subject_list, $groups_list, $datesub_list, $counter_list, $meta_list) = $GLOBALS['xoopsDB']->fetchRow($listing)) {
 	$groups_list = explode(" ",$groups_list);
 	if (count(array_intersect($group,$groups_list)) > 0 ) {
 
@@ -44,40 +55,38 @@ if( $meta_title_list ) { $alt_subject_list=$meta_title_list; } else { $alt_subje
 		/* ----------------------------------------------------------------------- */
 		/*                            Display icons                                */
 		/* ----------------------------------------------------------------------- */
-		if ( $xoopsModuleConfig['tags'] ){
-			$time = time();
-            $startdate_list = (time()-(86400 * $xoopsModuleConfig['tags_new']));
+		if ($GLOBALS['xoopsModuleConfig']['tags']) {
+			$time           = time();
+            $startdate_list = ($time - (86400 * $GLOBALS['xoopsModuleConfig']['tags_new']));
 
+            $new_list = '';
 			if ($startdate_list < $datesub_list) {
-            	$datesub_list = formatTimestamp($datesub_list,'m');
-                $new_list = '&nbsp;<img src="assets/images/icon/new.gif" alt="'.$datesub_list_list.'" />';
-            } else {
-				$new_list ='';
+            	$datesub_list = formatTimestamp($datesub_list, 'm');
+                $new_list = '&nbsp;<img src="assets/images/icon/new.gif" alt="' . $datesub_list . '">';
          	}
 
-			if ( $counter_list >= $xoopsModuleConfig['tags_pop'] ) {
-				$pop_list = '&nbsp;<img src="assets/images/icon/pop.gif" alt="'.$counter_list.'&nbsp;'._READS.'" />';
-			} else {
-				$pop_list ='';
+         	$pop_list = '';
+			if ($counter_list >= $GLOBALS['xoopsModuleConfig']['tags_pop']) {
+				$pop_list = '&nbsp;<img src="assets/images/icon/pop.gif" alt="' . $counter_list . '&nbsp;' . _READS . '">';
 			}
 		}
-		$liste['tag'] = $pop_list.$new_list;
+		$liste['tag'] = $pop_list . $new_list;
 
 		/* ----------------------------------------------------------------------- */
 		/*                            List images                                  */
 		/* ----------------------------------------------------------------------- */
 		/*
-		if ( $image ) {
-			$logo =  '<img src="'.XOOPS_URL . '/'. $xoopsModuleConfig['sbuploaddir'] .'/'. $image.'" align="absmiddle" height="32" />';
+		if ($image) {
+			$logo =  '<img src="' . XOOPS_URL . '/'. $GLOBALS['xoopsModuleConfig']['sbuploaddir'] . '/' . $image . '" align="absmiddle" height="32">';
 		} else {
-			$logo =  '<img src="assets/images/blank.gif" align="absmiddle" height="32" />';
+			$logo =  '<img src="assets/images/blank.gif" align="absmiddle" height="32">';
 		}
 		*/
 
 		/* ----------------------------------------------------------------------- */
 		/*                            List pages                                   */
 		/* ----------------------------------------------------------------------- */
-		if ( $count_list >= $total_list ) {
+		if ($count_list >= $total_list) {
 			$liste['cols'] = 1;
 			$count_list    = 1;
 		} else {
@@ -87,13 +96,13 @@ if( $meta_title_list ) { $alt_subject_list=$meta_title_list; } else { $alt_subje
 		$subject_list = $myts->displayTarea($subject_list);
 
         if ($ids != $id) {
-			$liste['link'] = "<nobr>" .edito_createlink('content.php?id='.$ids, $subject_list, '', '', '', '', '', $alt_subject_list, $xoopsModuleConfig['url_rewriting'])."</nobr>";
+			$liste['link'] = "<nobr>" .edito_createlink('content.php?id='.$ids, $subject_list, '', '', '', '', '', $alt_subject_list, $GLOBALS['xoopsModuleConfig']['url_rewriting'])."</nobr>";
                         // "<a href='content.php?id=$ids' alt='".$alt_subject."'><nobr>" . $subject . "</nobr></a>";
 		} else {
-			$liste['link'] = "<nobr><i>". $subject_list . "</i></nobr>";
+			$liste['link'] = "<nobr><i>" . $subject_list . "</i></nobr>";
    		}
 
-        $xoopsTpl->append('liste', $liste);
+        $GLOBALS['xoopsTpl']->append('liste', $liste);
         unset($liste);
 	}
 }
