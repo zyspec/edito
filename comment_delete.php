3608ea2724a8e1c31efc27bd8f6860b2e56acb1d
zyspec <zyspec@yahoo.com>
2020-07-08 09:50:33 -0500

    Minor cleanup of comment files
    * phpDocumentor updates
    * use Xmf\Request for input var sanitation

--- comment_delete.php
+++ comment_delete.php
@@ -1,28 +1,24 @@
 <?php
-// $Id: comment_delete.php,v 1.1 2004/01/29 14:45:23 buennagel Exp $
-//  ------------------------------------------------------------------------ //
-//                XOOPS - PHP Content Management System                      //
-//                    Copyright (c) 2000 XOOPS.org                           //
-//                       <http://www.xoops.org/>                             //
-//  ------------------------------------------------------------------------ //
-//  This program is free software; you can redistribute it and/or modify     //
-//  it under the terms of the GNU General Public License as published by     //
-//  the Free Software Foundation; either version 2 of the License, or        //
-//  (at your option) any later version.                                      //
-//                                                                           //
-//  You may not change or alter any portion of this comment or credits       //
-//  of supporting developers from this source code or any supporting         //
-//  source code which is considered copyrighted (c) material of the          //
-//  original comment or credit authors.                                      //
-//                                                                           //
-//  This program is distributed in the hope that it will be useful,          //
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of           //
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
-//  GNU General Public License for more details.                             //
-//                                                                           //
-//  You should have received a copy of the GNU General Public License        //
-//  along with this program; if not, write to the Free Software              //
-//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA //
-//  ------------------------------------------------------------------------ //
-include '../../mainfile.php';
-include XOOPS_ROOT_PATH.'/include/comment_delete.php';
+/*
+ You may not change or alter any portion of this comment or credits of
+ supporting developers from this source code or any supporting source code
+ which is considered copyrighted (c) material of the original comment or credit
+ authors.
+
+ This program is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+/**
+ * Module: Edito
+ *
+ * @package   \XoopsModules\Edito
+ * @copyright Copyright {@link https://xoops.org XOOPS Project}
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html GNU Public License
+ * @author    Solo (http://www.wolfpackclan.com/wolfactory)
+ * @author    DuGris (http://www.dugris.info)
+ * @author    XOOPS Module Development Team
+ * @link      https://github.com/XoopsModules25x/edito
+ */
+include dirname(__DIR__, 2) . '/mainfile.php';
+include XOOPS_ROOT_PATH . '/include/comment_delete.php';
--- comment_new.php
+++ comment_new.php
@@ -1,66 +1,59 @@
 <?php
-// $Id: comment_new.php,v 1.1 2004/01/29 14:45:23 buennagel Exp $
-//  ------------------------------------------------------------------------ //
-//                XOOPS - PHP Content Management System                      //
-//                    Copyright (c) 2000 XOOPS.org                           //
-//                       <http://www.xoops.org/>                             //
-//  ------------------------------------------------------------------------ //
-//  This program is free software; you can redistribute it and/or modify     //
-//  it under the terms of the GNU General Public License as published by     //
-//  the Free Software Foundation; either version 2 of the License, or        //
-//  (at your option) any later version.                                      //
-//                                                                           //
-//  You may not change or alter any portion of this comment or credits       //
-//  of supporting developers from this source code or any supporting         //
-//  source code which is considered copyrighted (c) material of the          //
-//  original comment or credit authors.                                      //
-//                                                                           //
-//  This program is distributed in the hope that it will be useful,          //
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of           //
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
-//  GNU General Public License for more details.                             //
-//                                                                           //
-//  You should have received a copy of the GNU General Public License        //
-//  along with this program; if not, write to the Free Software              //
-//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA //
-//  ------------------------------------------------------------------------ //
-include '../../mainfile.php';
-$com_itemid = isset($_GET['com_itemid']) ? intval($_GET['com_itemid']) : 0;
-if ($com_itemid > 0) {
-  $myts = MyTextSanitizer::getInstance();
+/*
+ You may not change or alter any portion of this comment or credits of
+ supporting developers from this source code or any supporting source code
+ which is considered copyrighted (c) material of the original comment or credit
+ authors.
 
-$sql=" SELECT subject, uid, image, block_text, body_text, options, datesub FROM ".$xoopsDB->prefix("edito_content")." WHERE id=$com_itemid AND status > 0";
-$result = $xoopsDB->queryF($sql);
-$myrow 	= $xoopsDB->fetchArray($result);
-$info = array();
+ This program is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+/**
+ * Module: Edito
+ *
+ * @package   \XoopsModules\Edito
+ * @copyright Copyright {@link https://xoops.org XOOPS Project}
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html GNU Public License
+ * @author    Solo (http://www.wolfpackclan.com/wolfactory)
+ * @author    DuGris (http://www.dugris.info)
+ * @author    XOOPS Module Development Team
+ * @link      https://github.com/XoopsModules25x/edito
+ */
 
-$image = $myrow["image"];
+use Xmf\Request;
 
-$option = explode("|", $myrow["options"]);
-$html            =  $option[0];
-$xcode           =  $option[1];
-$smiley          =  $option[2];
-$logo            =  $option[3];
-$block           =  $option[4];
-$title           =  $option[5];
-$cancomment      =  $option[6];
+include dirname(__DIR__, 2) . '/mainfile.php';
+$com_itemid = Request::getInt('com_itemid', 0);
+if (0 < $com_itemid) {
+    $myts   = MyTextSanitizer::getInstance();
+    $sql    ="SELECT subject, uid, image, block_text, body_text, options, datesub FROM " . $GLOBALS['xoopsDB']->prefix('edito_content') . " WHERE id={$com_itemid} AND status > 0";
+    $result = $GLOBALS['xoopsDB']->queryF($sql);
+    $myrow 	= $GLOBALS['xoopsDB']->fetchArray($result);
+    $image      = $myrow['image'];
+    $info       = [];
+    $option     = explode('|', $myrow['options']);
+    $html       = $option[0];
+    $xcode      = $option[1];
+    $smiley     = $option[2];
+    $logo       = $option[3];
+    $block      = $option[4];
+    $title      = $option[5];
+    $cancomment = $option[6];
 
-	if ( $image ) {
-                include_once("include/functions_content.php");
-		$logo =  $xoopsModuleConfig['sbuploaddir'] .'/'. $image;
-		$image_size = explode('|', $xoopsModuleConfig['logo_size']);
-		$logo = edito_createlink('', '', '', $logo, $xoopsModuleConfig['logo_align'], '800', '600', $myrow["subject"]);
-	} else {
-		$logo = '';
+    $logo = '';
+	if ($image) {
+        require_once __DIR__ . '/include/functions_content.php';
+		$logo       = $GLOBALS['xoopsModuleConfig']['sbuploaddir'] . '/' . $image;
+		$image_size = explode('|', $GLOBALS['xoopsModuleConfig']['logo_size']);
+		$logo       = edito_createlink('', '', '', $logo, $GLOBALS['xoopsModuleConfig']['logo_align'], '800', '600', $myrow['subject']);
 	}
 
+    $text = $block ? $myrow["block_text"] : '';
+    $text .= $myrow["body_text"];
 
-$text = '';
-if($block) { $text .= $myrow["block_text"]; }
-$text .= $myrow["body_text"];
+    $com_replytext  = _POSTEDBY . '&nbsp;<b>' . XoopsUser::getUnameFromId($myrow['uid']) . '</b>&nbsp;' . _DATE . '&nbsp;<b>' . formatTimestamp($myrow["datesub"],'m').'</b><br /><br />'.$logo . $myts->displayTarea($text, $html, $smiley, $xcode);
+    $com_replytitle = $myrow['subject'];
 
-        $com_replytext = _POSTEDBY.'&nbsp;<b>'.XoopsUser::getUnameFromId($myrow['uid']).'</b>&nbsp;'._DATE.'&nbsp;<b>'.formatTimestamp($myrow["datesub"],'m').'</b><br /><br />'.$logo.$myts->displayTarea($text, $html, $smiley, $xcode);
-        $com_replytitle = $myrow["subject"];
-
-	include_once XOOPS_ROOT_PATH.'/include/comment_new.php';
+	require_once XOOPS_ROOT_PATH . '/include/comment_new.php';
 }
